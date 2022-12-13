f5e51b333a3ee3424c66dd5e04194e1201719aca
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-13 12:38:30 +0100

    inclusion taps

--- lib/Players/Player.dart
+++ lib/Players/Player.dart
@@ -1,18 +1,34 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 
-class Player extends PositionComponent {
+class Player extends PositionComponent with Tappable {
   //static final _paint = Paint()..color = Colors.white;
   static final _paint = Paint();
-  late double x;
-  late double y;
-  late double widht;
-  late double heigt;
+  Color cVisisble =Colors.white;
 
   @override
   void render(Canvas canvas) {
-    _paint.color=Colors.pinkAccent;
+    _paint.color=Colors.white;
     //Rect rect=Rect.
     canvas.drawRect(size.toRect(), _paint);
   }
+  @override
+  bool onTapUp(TapUpInfo info){
+    print('tap up');
+    cVisisble=Colors.white;
+    return true;
+  }
+  @override
+  bool onTapDown(TapDownInfo info){
+    print('tap up');
+    cVisisble=Colors.red;
+    return super.onTapDown(info);
+  }
+  @override
+  void update(double dt) {
+    // TODO: implement update
+    super.update(dt);
+  }
+
 }
--- lib/Scenes/SpaceShooterGame.dart
+++ lib/Scenes/SpaceShooterGame.dart
@@ -2,17 +2,25 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutterkombat/Players/Player.dart';
 
-class SpaceShooterGame extends FlameGame{
+class SpaceShooterGame extends FlameGame with HasTappables{
+  Player nave= new Player();
+
+  SpaceShooterGame(){
+        nave.anchor = Anchor.center;
+  }
+@override
+  void onGameResize(Vector2 canvasSize) {
+    // TODO: implement onGameResize
+    super.onGameResize(canvasSize);
+    nave.position = this.size / 2; //tamaño pantalla
+    nave.width = size[0]*0.5;
+    nave.height = size[1]*0.3;
+}
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    this.add(nave);
 
-    add(
-      Player()
-        ..position = size / 2
-        ..width = 50
-        ..height = 100
-        ..anchor = Anchor.center,
-    );
   }
 }
