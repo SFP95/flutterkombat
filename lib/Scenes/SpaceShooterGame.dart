4c50d1e0aa00a13b72224b4078080d4760aa45a6
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-13 08:55:14 +0100

    añadimos el objeto nave

--- lib/Scenes/SpaceShooterGame.dart
+++ lib/Scenes/SpaceShooterGame.dart
@@ -1,5 +1,18 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flutterkombat/Players/Player.dart';
 
 class SpaceShooterGame extends FlameGame{
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
 
+    add(
+      Player()
+        ..position = size / 2
+        ..width = 50
+        ..height = 100
+        ..anchor = Anchor.center,
+    );
+  }
 }
