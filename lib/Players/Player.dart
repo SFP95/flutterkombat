7691e457785d3c377fbed8eaa2efb563950a2fbd
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-13 09:17:05 +0100

    añadido de parametros

--- lib/Players/Player.dart
+++ lib/Players/Player.dart
@@ -2,10 +2,17 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class Player extends PositionComponent {
-  static final _paint = Paint()..color = Colors.white;
+  //static final _paint = Paint()..color = Colors.white;
+  static final _paint = Paint();
+  late double x;
+  late double y;
+  late double widht;
+  late double heigt;
 
   @override
   void render(Canvas canvas) {
+    _paint.color=Colors.pinkAccent;
+    //Rect rect=Rect.
     canvas.drawRect(size.toRect(), _paint);
   }
 }
