9c462df13de2553e77bdb56dfc87fdc67713a69f
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-13 13:24:28 +0100

    inclusion rebotes  pantalla y taps

--- lib/Players/Player.dart
+++ lib/Players/Player.dart
@@ -8,6 +8,12 @@ class Player extends PositionComponent with Tappable {
   Color cVisible =Colors.white;
   double iYMovido=0;
   double iYOrigen=0;
+  int tapCount=0;
+  int iLevel=0;
+  int iDireccion=-2;
+  int altMax=800;
+  double gravedad=9.8;
+
 
   @override
   void render(Canvas canvas) {
@@ -17,20 +23,38 @@ class Player extends PositionComponent with Tappable {
   bool onTapUp(TapUpInfo info){
     print('tap up');
     cVisible=Colors.white;
+    position[1]+position[1]+iDireccion;
+
+    // if (iYMovido==350) {
+    //   iYMovido -= 100;
+    // }
     return true;
   }
   @override
   bool onTapDown(TapDownInfo info){
     print('tap up');
     cVisible=Colors.red;
-    iYMovido+=10; //movimiento del objeto
+    //iYMovido+=10; //movimiento del objeto
+    iDireccion=-iDireccion;
+    // tapCount++;
+    // if(tapCount>iLevel*5){
+    //   iDireccion=iDireccion*2;
+    //   tapCount=0;
+    // }
     return super.onTapDown(info);
   }
   @override
   void update(double dt) {
     super.update(dt);
     _paint.color=cVisible;
+    position[1]=position[1]+iDireccion+gravedad;
+    if(position[1]<0){
+      iDireccion=-iDireccion;
+    }
+    if(position[1]>altMax){
+      iDireccion=-iDireccion;
+    }
     //posicioón origen del objeto Player
-    position[1]=iYOrigen+iYMovido;
+    // position[1]=iYOrigen+iYMovido;
   }
 }
