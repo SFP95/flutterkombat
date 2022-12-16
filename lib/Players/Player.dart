2bea2850861d84acca197526eb4b4a4603bd2925
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-16 09:19:06 +0100

    cambiar cuadarado por cohete

--- lib/Players/Player.dart
+++ lib/Players/Player.dart
@@ -1,3 +1,4 @@
+import 'package:flame/cache.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
@@ -13,17 +14,41 @@ class Player extends PositionComponent with Tappable {
   int iDireccion=-2;
   int altMax=800;
   double gravedad=9.8;
+  late Rect rectObjecto;
+  late dynamic imgNave;
+  final imagesLoader = Images();
+  late Offset imgOffset;
+  late SpriteAnimation a;
 
+  @override
+  Future<void>? onLoad() async {
+    // TODO: implement onLoad
+    iYOrigen=position[1];
+    rectObjecto=size.toRect();
+
+    imgNave = (await imagesLoader.load('assets/icon/cohete.jpg'));
+    imgOffset = const Offset( 0,0);
+    sprite = Sprite(imgNave);
+
+    //spriteNave = Sprite(imgNave,srcSize: size);
+    //sprite = (await imagesLoader.load('naves/nave1.jpg'));
+    //add(spriteNave);
+    //imgNave=Image.asset("assets/icon/icon.png");
+    return super.onLoad();
+
+  }
 
   @override
   void render(Canvas canvas) {
-    canvas.drawRect(size.toRect(), _paint);
+    a.re
+    //sprite?.renderRect(canvas, rectObjecto);
+   // canvas.drawRect(size.toRect(), _paint);
   }
   @override
   bool onTapUp(TapUpInfo info){
     print('tap up');
     cVisible=Colors.white;
-    position[1]+position[1]+iDireccion;
+    //position[1]+position[1]+iDireccion;
 
     // if (iYMovido==350) {
     //   iYMovido -= 100;
@@ -47,7 +72,8 @@ class Player extends PositionComponent with Tappable {
   void update(double dt) {
     super.update(dt);
     _paint.color=cVisible;
-    position[1]=position[1]+iDireccion+gravedad;
+    position[1]=position[1]+iDireccion;
+    // position[1]=position[1]+iDireccion+gravedad;
     if(position[1]<0){
       iDireccion=-iDireccion;
     }
