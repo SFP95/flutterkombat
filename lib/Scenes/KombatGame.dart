fb0ca2682130c48aad1508f83925b3cdc01e0dd7
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-19 14:14:15 +0100

    añadido de KombatGame

---
+++ lib/Scenes/KombatGame.dart
@@ -0,0 +1,10 @@
+import 'package:flame/game.dart';
+
+class KombatGame extends FlameGame{
+
+@override
+  Future<void>? onLoad() async{
+    // TODO: implement onLoad
+    await super.onLoad();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterkombat/Scenes/KombatGame.dart';
 
 import 'Scenes/SpaceShooterGame.dart';
 
 void main() {
 
-
-  SpaceShooterGame game =SpaceShooterGame();
+  KombatGame game= KombatGame();
+  //SpaceShooterGame game =SpaceShooterGame();
   GameWidget gameWidget= GameWidget(game: game);
   runApp(gameWidget);
 
