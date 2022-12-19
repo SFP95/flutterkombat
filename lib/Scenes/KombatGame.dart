7addc8d9fadb8a20800382eee4ad860d4c5e9600
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-19 14:18:26 +0100

    añadido de KombatGame

--- lib/Scenes/KombatGame.dart
+++ lib/Scenes/KombatGame.dart
@@ -1,10 +1,13 @@
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 
 class KombatGame extends FlameGame{
-
+ late TiledComponent mapComponent;
 @override
   Future<void>? onLoad() async{
     // TODO: implement onLoad
     await super.onLoad();
+
+    mapComponent = await TiledComponent.load("mapa.tmx", Vector2.all(32));
   }
 }
