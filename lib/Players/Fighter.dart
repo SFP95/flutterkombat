557ac4600f1482f8b607e26c41b13a7e3e82e16c
Nia <103027252+SFP95@users.noreply.github.com>
2022-12-21 08:31:38 +0100

    ajuste y cambios

---
+++ lib/Players/Fighter.dart
@@ -0,0 +1,48 @@
+
+
+import 'package:flutterkombat/Scenes/KombatGame.dart';
+import 'package:flame_forge2d/body_component.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:forge2d/src/dynamics/body.dart';
+
+class Fighter extends BodyComponent<KombatGame>{
+
+  final Vector2 position;
+  final Vector2 size;
+
+  Fighter({required this.position, required this.size}){
+    renderBody=true;
+  }
+
+  @override
+  Body createBody() {
+
+    final shape = PolygonShape();
+
+    final vertices = [
+      Vector2(0, 0),
+      Vector2(size.x*0.65, 0),
+      Vector2(size.x*0.65, size.y),
+      Vector2(0, size.y),
+    ];
+    shape.set(vertices);
+
+    final fixtureDef = FixtureDef(
+      shape,
+      userData: this, // To be able to determine object in collision
+      restitution: 0.8,
+      density: 1.0,
+      friction: 0.2,
+    );
+
+    final velocity = (Vector2.random() - Vector2.random()) * 200;
+    BodyDef def=BodyDef(
+      type: BodyType.dynamic,
+      position: position,
+      //angle: velocity.angleTo(Vector2(1, 0)),
+      //linearVelocity: velocity
+    );
+
+    return world.createBody(def)..createFixture(fixtureDef);
+  }
+}
--- lib/Scenes/KombatGame.dart
+++ lib/Scenes/KombatGame.dart
@@ -2,22 +2,97 @@ import 'dart:ui';
 
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flame_forge2d/forge2d_game.dart';
 
-class KombatGame extends FlameGame{
+class KombatGame extends Forge2DGame{
 
- late TiledComponent mapComponent;
+  late TiledComponent mapComponent;
 
-@override
+  KombatGame():super(gravity: Vector2(0, 9.8), zoom: 1);
+
+  @override
   Future<void>? onLoad() async{
     // TODO: implement onLoad
     await super.onLoad();
 
     mapComponent = await TiledComponent.load('mapa.tmx', Vector2.all(32));
     add(mapComponent);
+
+
+    final capaObjGroup = mapComponent.tileMap.getLayer<ObjectGroup>('objetos');
+
+    for (final obj in capaObjGroup!.objects) {
+      addTiledObjectCollision(obj);
+    }
+
+    Fighter fighter=Fighter(position: Vector2(size.x/3,0), size: size*0.1);
+    add(fighter);
+
+
   }
- @override
- Color backgroundColor() {
-   // TODO: implement backgroundColor
-   return const Color.fromRGBO(104, 129, 255, 1.0);
- }
+
+  void addTiledObjectCollision(TiledObject tiledObject){
+
+    //DEFINO PRIMERO EL CUERPO Y NO LA FORMA
+    final bodyDef = BodyDef()
+      ..position = Vector2(tiledObject.x, tiledObject.y)
+      ..type = BodyType.static;
+    Body body=world.createBody(bodyDef);
+
+
+    //DEFINO LA FORMA
+
+    if(tiledObject.isPolygon) {
+      ChainShape shape = ChainShape();
+
+      final List<Vector2> vertices = [];
+      for (final polygon in tiledObject.polygon) {
+        //print("DEBUG: POINTS ----->>>>> "+polygon.x.toString()+"   "+polygon.y.toString());
+        //vertices.add(Vector2(polygon.x, polygon.y));
+        shape.vertices.add(Vector2(polygon.x, polygon.y));
+      }
+      final fixtureDef = FixtureDef(shape)
+        ..userData = this // To be able to determine object in collision
+        ..restitution = 0.8
+        ..density = 1.0
+        ..friction = 0.2;
+
+      body.createFixture(fixtureDef);
+    }
+    else if(tiledObject.isRectangle){
+      print("DEBUG: ---->>>> "+tiledObject.x.toString());
+      final shape = PolygonShape();
+
+      final vertices = [
+        /*Vector2(-size.x / 2, size.y / 2),
+      Vector2(size.x / 2, size.y / 2),
+      Vector2(0, -size.y / 2),*/
+        Vector2(0, 0),
+        Vector2(tiledObject.width, 0),
+        Vector2(tiledObject.width, tiledObject.height),
+        Vector2(0, tiledObject.height),
+      ];
+      shape.set(vertices);
+
+      //final shape = CircleShape()..radius = size.x / 2;
+      final fixtureDef = FixtureDef(shape)
+        ..userData = this // To be able to determine object in collision
+        ..restitution = 0.8
+        ..density = 1.0
+        ..friction = 0.2;
+
+      body.createFixture(fixtureDef);
+    }
+    //else if(tiledObject.isPolygon) {
+  }
+
+  @override
+  Color backgroundColor() {
+    // TODO: implement backgroundColor
+    return const Color.fromRGBO(104, 129, 255, 1.0);
+  }
+
+
+
 }
